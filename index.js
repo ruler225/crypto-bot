9d50072014236a2bd4fb22f5cb2b1205dad2a734
Youssef Mikhail <youssef.mikhail@outlook.com>
2021-01-13 01:58:08 -0500

    refactor - Switched from symbols to currency names, various bug fixes

--- index.js
+++ index.js
@@ -15,16 +15,18 @@ var coinFailMsg = [];
 const client = new Discord.Client();
 client.login(config.BOT_TOKEN);
 const prefix = "!";
-const baseURL = "https://web-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=";
+const baseURL = "https://web-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=";
 
 var data;
 var saves = {
     lastChannelId: "",
-    coinSymbols: [],
+    coinSlugs: [],
     alertThresholds: [],
     lastPriceNotified: [],
-    lastDateNotified: []
+    lastDateNotified: [],
+    coinNames: []
 };
+
 
 function writeCallback(err) {
     if (err) throw err;
@@ -40,30 +42,30 @@ function sleep(ms) {
     return new Promise((resolve) => {setTimeout(resolve, ms)});
 }
 
-function getCoinInfo(symbol) {
-    return new Promise((resolve) => {request.get({url: baseURL + symbol,
+function getCoinInfo(slug) {
+    return new Promise((resolve) => {request.get({url: baseURL + slug,
         json: true,
         headers: {}}, async (err, res, data) => {
             if (err) {
                 console.error(err);
                 resolve(-1);
-        } else if (res.statusCode != 200) {
+        } else if (res.statusCode != 200 && res.statusCode != 400) {
             console.error("Error: Non-OK status received: " + res.statusCode);
             resolve(-1);
         } else {
-            if (!data.data[symbol]) {
+            if (!data.data) {
                 resolve(-2);
             } else {
-                resolve(data.data[symbol]);
+                resolve(Object.values(data.data)[0]);
             }
         }
         resolve(-1);
     })});
 }
 
-async function handlePriceCheck(symbol) {
-    coinData = await getCoinInfo(symbol);
-    coinIndex = saves.coinSymbols.indexOf(symbol);
+async function handlePriceCheck(slug) {
+    coinData = await getCoinInfo(slug);
+    coinIndex = saves.coinSlugs.indexOf(slug);
         if (coinData == -1) {
             if (fail > 1) {
                 if (!failMsg)
@@ -74,7 +76,7 @@ async function handlePriceCheck(symbol) {
             if (coinData == -2) {
 		        if (coinFail[coinIndex] > 1) {
                     		if (!coinFailMsg[coinIndex])
-					coinFailMsg[coinIndex] = await client.channels.cache.get(saves.lastChannelId).send("I was unable to fetch price info for " + symbol + ". I'll keep trying and I will edit this message once I am able to successfully do so.");
+					coinFailMsg[coinIndex] = await client.channels.cache.get(saves.lastChannelId).send("I was unable to fetch price info for " + saves.coinNames[coinIndex] + ". I'll keep trying and I will edit this message once I am able to successfully do so.");
 			}
 		        coinFail[coinIndex] += 1;
                 return;
@@ -108,10 +110,10 @@ async function handlePriceCheck(symbol) {
 
             if (alertPrice) {
                 if (difference < 0 ) {
-                    client.channels.cache.get(saves.lastChannelId).send(symbol + " PRICE CHANGE ALERT: Price is now " + price + " USD, a decrease of " +
+                    client.channels.cache.get(saves.lastChannelId).send(saves.coinNames[coinIndex] + " PRICE CHANGE ALERT: Price is now " + price + " USD, a decrease of " +
                         Math.abs(difference) + " USD (" + percentageDifference.toFixed(1) + "%) since " + saves.lastDateNotified[coinIndex].toLocaleTimeString([], {day:'numeric', month:'numeric',year:'numeric', hour:'numeric', minute:'numeric'}));
                 } else {
-                    client.channels.cache.get(saves.lastChannelId).send(symbol + " PRICE CHANGE ALERT: Price is now " + price + " USD, an increase of " +
+                    client.channels.cache.get(saves.lastChannelId).send(saves.coinNames[coinIndex] + " PRICE CHANGE ALERT: Price is now " + price + " USD, an increase of " +
                         difference + " USD (" + percentageDifference.toFixed(1) + "%) since " + saves.lastDateNotified[coinIndex].toLocaleTimeString([], {day:'numeric', month:'numeric',year:'numeric', hour:'numeric', minute:'numeric'}));
                 }
                 saves.lastPriceNotified[coinIndex] = price;
@@ -122,6 +124,33 @@ async function handlePriceCheck(symbol) {
         }
     
 }
+
+//Sometimes (inexplicably) the discord.js module will throw an UnhandledPromiseRejection. Since I have no way of handling this myself, I need to manually crash the program when this happens
+process.on('unhandledRejection', (reason, p) => {
+    console.error(reason);
+if (ready) {
+    try {
+    client.channels.cache.get(saves.lastChannelId).send("A problem occurred and I had to terminate. I should be restarting very shortly though. Please refer to the developer log for crash details");
+    } catch (err) {
+        console.error(err);
+        process.exit(-1);
+    }
+}
+process.exit(-1);
+});
+
+process.on('uncaughtException', (err, origin) => {
+console.error(err);
+if (ready) {
+    try {
+        client.channels.cache.get(saves.lastChannelId).send("A problem occurred and I had to terminate. I should be restarting very shortly though. Please contact the developer for crash details.");
+    } catch (err) {
+        console.error(err);
+        process.exit(-1);
+    }
+}
+process.exit(-1);
+});
 
 //Load configuration data
 try {
@@ -143,35 +172,8 @@ try {
 }
 
 //Initialize fail data
-coinFail = Array(saves.coinSymbols.length).fill(0);
-coinFailMsg = Array(saves.coinSymbols.length).fill(undefined);
-
-//Sometimes (inexplicably) the discord.js module will throw an UnhandledPromiseRejection. Since I have no way of handling this myself, I need to manually crash the program when this happens
-process.on('unhandledRejection', (reason, p) => {
-    	console.error(reason);
-	if (ready) {
-		try {
-		client.channels.cache.get(saves.lastChannelId).send("A problem occurred and I had to terminate. I should be restarting very shortly though. Please refer to the developer log for crash details");
-		} catch (err) {
-			console.error(err);
-			process.exit(-1);
-		}
-	}
-	process.exit(-1);
-  });
-
-process.on('uncaughtException', (err, origin) => {
-	console.error(err);
-	if (ready) {
-		try {
-			client.channels.cache.get(saves.lastChannelId).send("A problem occurred and I had to terminate. I should be restarting very shortly though. Please contact the developer for crash details.");
-		} catch (err) {
-			console.error(err);
-			process.exit(-1);
-		}
-	}
-	process.exit(-1);
-});
+coinFail = Array(saves.coinSlugs.length).fill(0);
+coinFailMsg = Array(saves.coinSlugs.length).fill(undefined);
 
 client.on("ready", function() {
     if (saves.lastChannelId == "") {
@@ -185,14 +187,17 @@ client.on("ready", function() {
                     return true;
                 }
                 //client.channels.cache.get(saves.lastChannelId).send("Hello! I'm now up and running! I will be sending all of my updates to this channel from now on. " +
-                //    "If you would like to change this, you can go to the channel you would like me to move to and type `!setActiveChannel`. " +
-                //    "For a list of commands, type `!help`.");
+                 //   "If you would like to change this, you can go to the channel you would like me to move to and type `!setActiveChannel`. " +
+                 //   "For a list of commands, type `!help`.");
                 return false;
             });
     });
     }
     ready = true;
-    client.user.setActivity(saves.coinSymbols.length + " cryptocurrencies", {type: 'WATCHING'});
+    if (saves.coinSlugs.length == 1)
+        client.user.setActivity(saves.coinSlugs.length + " cryptocurrency", {type: 'WATCHING'});
+    else
+        client.user.setActivity(saves.coinSlugs.length + " cryptocurrencies", {type: 'WATCHING'});
 });
 
 client.on("message", async function(message) {
@@ -223,85 +228,104 @@ else if (command == "help") {
     message.channel.send("Here are a list of commands I support: \n\n" + 
         "`!help`: show this help menu\n" + 
         "`!setActiveChannel`: Sends all future notifications to the channel that you use this command in.\n" +  
-        "`!watch <crypto-symbol> (<percentage-change>% | <USD-change>)`: Watch a specified crypto currency and notify the active channel when the price changes by the specified percentage/amount in USD. To specify a percentage threshold, add a percent sign to the end of your number. To specify a USD threshold, just enter the number without any other symbols.\n" +
-        "`!remove <crypto-symbol>`: Stops watching a specified cryptocurrency.\n" + 
+        "`!watch <currency-name> (<percentage-change>% | <USD-change>)`: Watch a specified crypto currency and notify the active channel when the price changes by the specified percentage/amount in USD. To specify a percentage threshold, add a percent sign to the end of your number. To specify a USD threshold, just enter the number without any other slugs.\n" +
+        "`!remove <currency-name>`: Stops watching a specified cryptocurrency.\n" + 
         "`!list`: Lists all cryptocurrencies that are currently being watched, and their price fluctuation thresholds.\n" + 
-        "`!check <crypto-symbol>`: Checks the price of a specified cryptocurrency in USD.");
+        "`!check <currency-name>`: Checks the price of a specified cryptocurrency in USD.");
 }
 else if (command == "watch") {
     if (args.length == 0) {
-        message.channel.send("Usage: `!watch <symbol> (<percentage-change>% | <USD-change>)`. \nExample percentage command: `!watch BTC 20%` (this watches Bitcoin and sends an alert when the price fluctuates by more than 20%)\n" + 
-            "Example absolute amount command: `!watch BTC 30` (this alerts you when the price of BTC fluctuates by 30 USD)");
+        message.channel.send("Usage: `!watch <currency-name> (<percentage-change>% | <USD-change>)`. \nExample percentage command: `!watch bitcoin 20%` (this watches Bitcoin and sends an alert when the price fluctuates by more than 20%)\n" + 
+            "Example absolute amount command: `!watch bitcoin 30` (this alerts you when the price of BTC fluctuates by 30 USD)");
         return;
     } else if (args.length < 2) {
-        message.channel.send("You need to include a percentage change or currency amount to alert you of. \nExample command: `!watch BTC 20%` (this watches Bitcoin and sends an alert when the price fluctuates by more than 20%)\n" + 
-            "Example absolute amount command: `!watch BTC 30` (this alerts you when the price of BTC fluctuates by 30 USD)");
+        message.channel.send("You need to include a percentage change or currency amount to alert you of. \nExample command: `!watch bitcoin 20%` (this watches Bitcoin and sends an alert when the price fluctuates by more than 20%)\n" + 
+            "Example absolute amount command: `!watch bitcoin 30` (this alerts you when the price of BTC fluctuates by 30 USD)");
         return;
     }
-    if (!Number(args[1]) &&  !args[1].endsWith('%')) {
-        message.channel.send("\"" + args[1] + "\" is not a number or a percentage!\nExample percentage command: `!watch BTC 20%` (this watches Bitcoin and sends an alert when the price fluctuates by more than 20%)\n" +
-            "Example absolute amount command: `!watch BTC 30` (this alerts you when the price of BTC fluctuates by 30 USD)");
+    let threshold = args[args.length - 1];
+    if (!Number(threshold) && !threshold.endsWith('%')) {
+        message.channel.send("\"" + threshold + "\" is not a number or a percentage!\nExample percentage command: `!watch bitcoin 20%` (this watches Bitcoin and sends an alert when the price fluctuates by more than 20%)\n" +
+            "Example absolute amount command: `!watch bitcoin 30` (this alerts you when the price of bitcoin fluctuates by 30 USD)");
         return;
     }
 
-    const symbol = args[0].toUpperCase();
-    const coinIndex = saves.coinSymbols.indexOf(symbol);
+    const inputName = args.slice(0, args.length - 1).join(' ');
+    const slug = inputName.toLowerCase().replace(/-/g, "").replace(/ /g, "-"); //TODO: make sure this is immune to trailing/leading spaces
+    const coinIndex = saves.coinSlugs.indexOf(slug);
     if (coinIndex != -1 ) {
         let oldThreshold = saves.alertThresholds[coinIndex];
-        let newThreshold = args[1];
+        let newThreshold = threshold;
         if (!oldThreshold.endsWith('%')) {
             oldThreshold += " USD";
         }
         if (!newThreshold.endsWith('%')) {
             newThreshold += " USD";
         }
-        message.channel.send("Now setting alert threshold for " + symbol + " from " + oldThreshold + " to " + newThreshold);
-        saves.alertThresholds[coinIndex] = args[1];
+        message.channel.send("Now setting alert threshold for " + saves.coinNames[coinIndex] + " from " + oldThreshold + " to " + newThreshold);
+        saves.alertThresholds[coinIndex] = threshold;
         saveConfig();
     } else {
-        let threshold = args[1];
-        if (!symbol) {
-            message.channel.send("I didn't receive a proper symbol! Please try again.");
+        if (!slug) {
+            message.channel.send("I didn't receive a proper name! Please try again.");
             return;
         }
-        let coinData = await getCoinInfo(symbol);
+        let coinData = await getCoinInfo(slug);
         if (coinData == -1) {
             client.channels.cache.get(saves.lastChannelId).send("There was a problem connecting to coinmarketcap's servers. Please see developer log for details.");
             console.error(err);
         } else {
             if (coinData == -2) {
-                message.channel.send("Couldn't find a coin with the symbol: " + symbol);
+                message.channel.send("Couldn't find a coin with the name: " + inputName);
                 return;
             }
             const price = coinData.quote.USD.price;
             const name = coinData.name;
-            saves.coinSymbols.push(symbol);
-            saves.alertThresholds.push(args[1]);
+            saves.coinSlugs.push(slug);
+            saves.alertThresholds.push(threshold);
             saves.lastPriceNotified.push(price);
             saves.lastDateNotified.push(new Date());
+            saves.coinNames.push(name);
+            coinFail.push(0);
+            coinFailMsg.push(undefined);
             if (!threshold.endsWith('%')) 
                 threshold += " USD";
-            message.channel.send("Now watching " + name + " (" + args[0].toUpperCase() + ") for price change of " + threshold);
-            client.user.setActivity(saves.coinSymbols.length + " cryptocurrencies", {type: 'WATCHING'});
+            message.channel.send("Now watching " + name + " for price change of " + threshold);
+            if (saves.coinSlugs.length == 1)
+                client.user.setActivity(saves.coinSlugs.length + " cryptocurrency", {type: 'WATCHING'});
+            else
+                client.user.setActivity(saves.coinSlugs.length + " cryptocurrencies", {type: 'WATCHING'});
+
 		    saveConfig();
         }
     }
 }
 else if (command == "remove") {
-    if (args.length != 1) {
-        message.channel.send("Usage: `!remove <symbol>`");
+    if (args.length < 1) {
+        message.channel.send("Usage: `!remove <currency-name>`");
         return;
     }
-    const coinIndex = saves.coinSymbols.indexOf(args[0].toUpperCase());
+    const inputName = args.join(' ');
+    const slug = inputName.toLowerCase().replace(/-/g, "").replace(/ /g, "-"); //TODO: make sure this is immune to trailing/leading spaces
+
+
+    const coinIndex = saves.coinSlugs.indexOf(slug);
+    const name = saves.coinNames[coinIndex];
     if (coinIndex != -1) {
-        saves.coinSymbols.splice(coinIndex, 1);
+        saves.coinSlugs.splice(coinIndex, 1);
         saves.alertThresholds.splice(coinIndex, 1);
         saves.lastPriceNotified.splice(coinIndex, 1);
         saves.lastDateNotified.splice(coinIndex, 1);
-	client.user.setActivity(saves.coinSymbols.length + " cryptocurrencies", {type: 'WATCHING'});
-        message.channel.send("Okay. I'm no longer watching " + args[0].toUpperCase());
+        saves.coinNames.splice(coinIndex, 1);
+        coinFail.splice(coinIndex, 1);
+        coinFailMsg.splice(coinIndex, 1);
+        if (saves.coinSlugs.length == 1)
+            client.user.setActivity(saves.coinSlugs.length + " cryptocurrency", {type: 'WATCHING'});
+        else
+            client.user.setActivity(saves.coinSlugs.length + " cryptocurrencies", {type: 'WATCHING'});
+        message.channel.send("Okay. I'm no longer watching " + name);
     } else {
-        message.channel.send("I'm currently not watching a coin called " + args[0].toUpperCase() );
+        message.channel.send("I'm currently not watching a coin called " + inputName);
     }
     saveConfig();
 }
@@ -315,34 +339,35 @@ else if (command == "debug") {
 }
 else if (command == "check") {
     if (!args[0]) {
-    	message.channel.send("You need to specify a symbol first. Example command: `!check BTC` checks the current price of Bitcoin in USD");
+    	message.channel.send("You need to specify a cryptocurrency first. Example command: `!check bitcoin` checks the current price of Bitcoin in USD");
 	    return;
     }
-    const symbol = args[0].toUpperCase();
-    let coinData = await getCoinInfo(symbol);
+
+    const inputName = args.join(' ');    
+    const slug = inputName.toLowerCase().replace(/-/g, "").replace(/ /g, "-"); //TODO: make sure this is immune to trailing/leading spaces
+    let coinData = await getCoinInfo(slug);
     if (coinData == -1) {
         client.channels.cache.get(saves.lastChannelId).send("There was a problem connecting to coinmarketcap's servers. Please check the developer log for details.");
-        console.error(err);
     } else {
         if (coinData == -2) {
-            message.channel.send("Couldn't find a coin with the symbol: " + symbol);
+            message.channel.send("Couldn't find a coin with the name: " + inputName);
             return;
         }
         const price = coinData.quote.USD.price;
         const name = coinData.name;
-        message.channel.send("The current price of " + name + " (" + symbol + ") is " + price + " USD");
+        message.channel.send("The current price of " + name + " is " + price + " USD");
     } 
 }
 else if (command == "list") {
-    if (saves.coinSymbols.length == 0) {
+    if (saves.coinSlugs.length == 0) {
         message.channel.send("I am currently not watching any currencies!");
     } else {
         var msgtext = "I am currently watching the following currencies: \n\n";
-        for (var i = 0; i < saves.coinSymbols.length; i++) {
+        for (var i = 0; i < saves.coinSlugs.length; i++) {
             let threshold = saves.alertThresholds[i];
             if (!threshold.endsWith('%'))
                 threshold += " USD";
-            msgtext += saves.coinSymbols[i] + " for a " + threshold + " change\n";
+            msgtext += saves.coinNames[i] + " for a " + threshold + " change\n";
         }
         message.channel.send(msgtext);
     }
@@ -356,10 +381,11 @@ if (message.channel.id != saves.lastChannelId) {
 
 mainLoop();
 async function mainLoop() {
+    await sleep(5000);
     while (true) {
-        for (var i = 0; i < saves.coinSymbols.length && ready; i++) {
-            const symbol = saves.coinSymbols[i];
-            handlePriceCheck(symbol);
+        for (var i = 0; i < saves.coinSlugs.length && ready; i++) {
+            const slug = saves.coinSlugs[i];
+            handlePriceCheck(slug);
         }
         await sleep(60000);
         
